a179067d0cb69ece79e97add001e24ceece14345
Nehal <panehala@sheridancollege.ca>
2023-05-18 14:07:21 -0400

    First Commit

---
+++ src/software1/ReverseString.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package software1;
+import java.util.*;
+import java.lang.String;
+/**
+ *
+ * @author Nehal Patel
+ */
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner read=new Scanner(System.in);
+                System.out.print("Enter any String: ");
+        String str=read.next();
+        char ar[]=new char[str.length()];
+        
+        for(int i=0;i<str.length();i++)
+        {
+            ar[i]=str.charAt(i);
+        }
+        //System.out.println(ar);
+
+        for(int i=ar.length-1;i>=0;i--)
+        {
+            System.out.print(ar[i]);
+        }
+        
+        
+        
+    }
+}
