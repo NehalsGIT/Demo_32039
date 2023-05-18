c8984a71547e36ada219841ebdc405105f5aa2d8
Nehal <panehala@sheridancollege.ca>
2023-05-18 14:45:47 -0400

    First Commit

--- src/software1/ReverseString.java
+++ src/software1/ReverseString.java
@@ -14,7 +14,7 @@ public class ReverseString {
     public static void main(String[] args) {
 
         Scanner read=new Scanner(System.in);
-                System.out.print("Enter any String to Reverse: ");
+                System.out.print("Enter any String: ");
         String str=read.next();
         char ar[]=new char[str.length()];
         
